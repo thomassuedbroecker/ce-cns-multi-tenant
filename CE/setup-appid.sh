5e756860504ef3c0eb5014eb458b56e9b381d652
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-09-24 12:01:46 +0200

    configure to list applications

--- CE/setup-appid.sh
+++ CE/setup-appid.sh
@@ -6,7 +6,8 @@
 # **************** Global variables
 
 export RESOURCE_GROUP=default
-export REGION="us-south"
+#export REGION="us-south"
+export REGION="eu-de"
 export SERVICE_PLAN="lite"
 export APPID_SERVICE_NAME="appid"
 #export YOUR_SERVICE_FOR_APPID="appID-multi-tenancy-example-tsuedbro"
@@ -22,13 +23,14 @@ export MANAGEMENTURL=""
 createAppIDService() {
     ibmcloud target -g $RESOURCE_GROUP
     ibmcloud target -r $REGION
-    ibmcloud catalog service-marketplace | grep $APPID_SERVICE_NAME
+    #List AppID service in marketplace
+    #ibmcloud catalog service-marketplace | grep $APPID_SERVICE_NAME
     # Create AppID service
     #ibmcloud resource service-instance-create $YOUR_SERVICE_FOR_APPID $APPID_SERVICE_NAME $SERVICE_PLAN $REGION
     # Show AppID service instance details
     ibmcloud resource service-instance $YOUR_SERVICE_FOR_APPID
     # Create a service key for the service
-    ibmcloud resource service-key-create $APPID_SERVICE_KEY_NAME $APPID_SERVICE_KEY_ROLE --instance-name $YOUR_SERVICE_FOR_APPID
+    #ibmcloud resource service-key-create $APPID_SERVICE_KEY_NAME $APPID_SERVICE_KEY_ROLE --instance-name $YOUR_SERVICE_FOR_APPID
     # Get existing service keys for the service
     ibmcloud resource service-keys --instance-name $YOUR_SERVICE_FOR_APPID
     # Get the details for the service keys for the service
@@ -46,13 +48,25 @@ getUsersAppIDService() {
     export OAUTHTOKEN=$(ibmcloud iam oauth-tokens | awk '{print $4;}')
     echo "Auth Token: $OAUTHTOKEN"
     # Invoke get users curl command
+    echo "-------------------------"
+    echo " List users"
+    echo "-------------------------"
     curl -i $MANAGEMENTURL/users -H "Authorization: Bearer $OAUTHTOKEN"
     # Invoke a export users curl command
+    echo "-------------------------"
+    echo " Export users"
+    echo "-------------------------"
     curl -i $MANAGEMENTURL/users/export -H "Authorization: Bearer $OAUTHTOKEN"
     # Invoke get redirect uris
+    echo "-------------------------"
+    echo " List uris"
+    echo "-------------------------"
     curl -i $MANAGEMENTURL/config/redirect_uris -H "Authorization: Bearer $OAUTHTOKEN"
     # Invoke get applications
-    curl -i $MANAGEMENTURL/applications
+    echo "-------------------------"
+    echo " List applications"
+    echo "-------------------------"
+    curl -i $MANAGEMENTURL/applications -H "Authorization: Bearer $OAUTHTOKEN"
 }
 
 
@@ -68,6 +82,8 @@ createAppIDService
 echo "************************************"
 echo " Get AppID Information "
 echo "************************************"
+
+getUsersAppIDService
 
 
 
